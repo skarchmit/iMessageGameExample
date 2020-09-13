8958fb46bb314e53a6648f91fd311001a5eb8c44
Sergey Karchmit <skarchmit@gmail.com>
2020-09-12 20:09:42 -0700

    Remove commented out variables, the game should be part of GameManager

--- iMessageGameExample MessagesExtension/NewGameScene.swift
+++ iMessageGameExample MessagesExtension/NewGameScene.swift
@@ -28,7 +28,7 @@ class NewGameScene: iMessageGame.Scene {
             if node.name == "newGameButton" {
                 if let d = gameDelegate {
                     print ("Creating new game")
-                    d.game = Game()
+                    self.gameManager.game = Game()
                     print ("sending game to game delegate.")
                     d.send(caption: "New Game", summaryText: "Sent a Game", withConfirmation: true)
                 }
